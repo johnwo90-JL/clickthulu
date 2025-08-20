d9ee1d3835ae1c99a907183d41796c98cadc7190
Tor-HodeKode <tor.kodehode@gmail.com>
2025-08-20 13:58:37 +0200

    Fixed glow

--- frontend/src/pages/cards.jsx
+++ frontend/src/pages/cards.jsx
@@ -2,46 +2,15 @@ import React, { useRef } from "react";
 import styles from "../styles/card.module.css";
 import { base_url } from "../../config";
 
-const cardNamesRaw = [
-  "Demon_common.png",
-  "Xpboost_common.png",
-  "Doctor_common.png",
-  "Demon_uncommon.png",
-  "Xpboost_uncommon.png",
-  "Doctor_uncommon.png",
-  "Demon_epic.png",
-  "Xpboost_epic.png",
-  "Doctor_epic.png",
-  "Castle_common.png",
-  "Castle_uncommon.png",
-  "Castle_epic.png",
-  "Finger_common.png",
-  "Finger_uncommon.png",
-  "Finger_epic.png",
-  "Wingydemon_common.png",
-  "Wingydemon_uncommon.png",
-  "Wingydemon_epic.png",
-  "Nokken_common.png",
-  "Nokken_uncommon.png",
-  "Nokken_epic.png",
-  "Oldman_common.png",
-  "Oldman_uncommon.png",
-  "Oldman_epic.png",
-];
-
 function getRarity(name) {
   if (name.includes("epic")) return "epic";
   if (name.includes("uncommon")) return "uncommon";
   return "common";
 }
 
-const rarityOrder = { common: 0, uncommon: 1, epic: 2 };
-const cardNames = [...cardNamesRaw].sort(
-  (a, b) => rarityOrder[getRarity(a)] - rarityOrder[getRarity(b)]
-);
-
 function Card({ name }) {
   const cardRef = useRef(null);
+  const rarity = getRarity(name);
 
   const handleMouseMove = (e) => {
     const card = cardRef.current;
@@ -63,8 +32,7 @@ function Card({ name }) {
     cardRef.current.style.transform = "";
   };
 
-  const rarity = getRarity(name);
-
+  // Bruker CSS-modul-klasser
   return (
     <div
       className={`${styles.card} ${styles[rarity]}`}
@@ -84,6 +52,32 @@ function Card({ name }) {
 }
 
 function Cards() {
+  const cardNames = [
+    "Demon_common.png",
+    "Xpboost_common.png",
+    "Doctor_common.png",
+    "Demon_uncommon.png",
+    "Xpboost_uncommon.png",
+    "Doctor_uncommon.png",
+    "Demon_epic.png",
+    "Xpboost_epic.png",
+    "Doctor_epic.png",
+    "Castle_common.png",
+    "Castle_uncommon.png",
+    "Castle_epic.png",
+    "Finger_common.png",
+    "Finger_uncommon.png",
+    "Finger_epic.png",
+    "Wingydemon_common.png",
+    "Wingydemon_uncommon.png",
+    "Wingydemon_epic.png",
+    "Nokken_common.png",
+    "Nokken_uncommon.png",
+    "Nokken_epic.png",
+    "Oldman_common.png",
+    "Oldman_uncommon.png",
+    "Oldman_epic.png",
+  ];
   return (
     <div className={styles.cardsContainer}>
       {cardNames.map((name) => (
